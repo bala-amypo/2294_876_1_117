4790dbf657e1ac7bddcf45475706ea484e16e5bf
bala-amypo <bala@amypo.in>
2025-12-18 10:12:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DeviceProfileRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.DeviceProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeviceProfileRepository extends JpaRepository<DeviceProfile, Long> {
+
+    DeviceProfile findByDeviceId(String deviceId);
+}
