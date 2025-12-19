ee2fdf3e927fa30cbc089e88b0a5fff4a26dbe50
bala-amypo <bala@amypo.in>
2025-12-19 05:08:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeviceProfile.java
+++ src/main/java/com/example/demo/entity/DeviceProfile.java
@@ -4,95 +4,80 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-
+@Table(name = "device_profile")
 public class DeviceProfile {
 
     @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique=true)
+
+    // Logical relation to UserAccount
     private Long userId;
 
-    @Column(unique=true)
     private String deviceId;
-
     private String deviceType;
-
     private String osVersion;
 
+    private Boolean isTrusted;
     private LocalDateTime lastSeen;
-
-    private Boolean isTrusted = false;
-
-public void setTrusted(boolean trusted) {
-    this.isTrusted = trusted;
-}
-
 
     public DeviceProfile() {
     }
 
-    public DeviceProfile(Long userId, String deviceId,
-                         String deviceType, String osVersion) {
-        this.id=id;
-        this.userId = userId;
-        this.deviceId = deviceId;
-        this.deviceType = deviceType;
-        this.osVersion = osVersion;
-    }
-
-    @PrePersist
-    @PreUpdate
-    protected void updateLastSeen() {
-        this.lastSeen = LocalDateTime.now();
-    }
+    // -------- Getters and Setters --------
 
     public Long getId() {
         return id;
     }
-
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public Long getUserId() {
         return userId;
     }
-
+    
     public void setUserId(Long userId) {
         this.userId = userId;
     }
-
+    
     public String getDeviceId() {
         return deviceId;
     }
-
+    
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
     }
-
+    
     public String getDeviceType() {
         return deviceType;
     }
-
+    
     public void setDeviceType(String deviceType) {
         this.deviceType = deviceType;
     }
-
+    
     public String getOsVersion() {
         return osVersion;
     }
-
+    
     public void setOsVersion(String osVersion) {
         this.osVersion = osVersion;
     }
-
-    public LocalDateTime getLastSeen() {
-        return lastSeen;
-    }
-
+    
     public Boolean getIsTrusted() {
         return isTrusted;
     }
-
-    public void setIsTrusted(Boolean trusted) {
-        isTrusted = trusted;
+    
+    public void setIsTrusted(Boolean isTrusted) {
+        this.isTrusted = isTrusted;
+    }
+    
+    public LocalDateTime getLastSeen() {
+        return lastSeen;
+    }
+    
+    public void setLastSeen(LocalDateTime lastSeen) {
+        this.lastSeen = lastSeen;
     }
 }
