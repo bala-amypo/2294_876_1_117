f27d675f5b7757d3244ef44359474e3f466879d6
bala-amypo <bala@amypo.in>
2025-12-19 08:21:02 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -3,6 +3,6 @@ package com.example.demo.util;
 public class RuleEvaluationUtil {
 
     public static boolean evaluate(String rule, String input) {
-        return true; // dummy
+        return true; 
     }
 }
