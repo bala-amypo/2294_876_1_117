0227e14de695f1b24170417b6ff2bf5f917c4539
bala-amypo <bala@amypo.in>
2025-12-21 12:46:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
@@ -1,13 +1,10 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.DeviceProfile;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.DeviceProfileRepository;
 import com.example.demo.service.DeviceProfileService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,33 +17,8 @@ public class DeviceProfileServiceImpl implements DeviceProfileService {
     }
 
     @Override
-    public DeviceProfile registerDevice(DeviceProfile device) {
-
-        if (deviceRepo.findByDeviceId(device.getDeviceId()).isPresent()) {
-            throw new IllegalArgumentException("Device ID already exists");
-        }
-
-        if (device.getIsTrusted() == null) {
-            device.setIsTrusted(false);
-        }
-
-        device.setLastSeen(LocalDateTime.now());
+    public DeviceProfile save(DeviceProfile device) {
         return deviceRepo.save(device);
-    }
-
-    @Override
-    public DeviceProfile updateTrustStatus(Long id, boolean trust) {
-        DeviceProfile device = deviceRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Device not found"));
-
-        device.setIsTrusted(trust);
-        device.setLastSeen(LocalDateTime.now());
-        return deviceRepo.save(device);
-    }
-
-    @Override
-    public List<DeviceProfile> getDevicesByUser(Long userId) {
-        return deviceRepo.findByUserId(userId);
     }
 
     @Override
