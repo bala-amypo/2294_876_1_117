2d4a9cb3513f052c2cb7ffb4db5a5d0398c74dc6
bala-amypo <bala@amypo.in>
2025-12-21 03:15:21 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -5,4 +5,16 @@ public class RuleEvaluationUtil {
     public static boolean evaluate(String rule, String input) {
         return true; 
     }
+}package com.example.demo.util;
+
+import com.example.demo.entity.LoginEvent;
+
+public class RuleEvaluationUtil {
+
+    public RuleEvaluationUtil() {
+    }
+
+    public void evaluateLoginEvent(LoginEvent event) {
+        // do nothing
+    }
 }
