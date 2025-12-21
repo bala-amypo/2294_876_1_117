4566db8a2ee08399a48ea4db1d4ae598c6f4c9f4
bala-amypo <bala@amypo.in>
2025-12-21 08:07:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    
     Optional<UserAccount> findByUsername(String username);
-    Optional<UserAccount> findByEmail(String email); // needed for unique email checks
+    
+    Optional<UserAccount> findByEmail(String email);
 }
--- src/main/java/com/example/demo/repository/ViolationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ViolationRecordRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ViolationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface ViolationRecordRepository extends JpaRepository<ViolationRecord, Long> {
