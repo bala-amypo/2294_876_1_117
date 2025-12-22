7d1cbd69df731c439c626fb6c812daa46e2aee09
bala-amypo <bala@amypo.in>
2025-12-22 00:58:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/LoginEventController.java
+++ src/main/java/com/example/demo/controller/LoginEventController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/logins")
+@RequestMapping("/api/logins")
 public class LoginEventController {
 
     private final LoginEventService loginService;
--- src/main/java/com/example/demo/controller/PolicyRuleController.java
+++ src/main/java/com/example/demo/controller/PolicyRuleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rules")
+@RequestMapping("/api/rules")
 public class PolicyRuleController {
 
     private final PolicyRuleService ruleService;
