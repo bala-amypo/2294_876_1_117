0f7a1fb1475c71055845aba862b7b097c4ce0de2
bala-amypo <bala@amypo.in>
2025-12-23 08:56:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceProfileRepository.java
+++ src/main/java/com/example/demo/repository/DeviceProfileRepository.java
@@ -2,12 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DeviceProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DeviceProfileRepository extends JpaRepository<DeviceProfile, Long> {
-
     Optional<DeviceProfile> findByDeviceId(String deviceId);
-
     List<DeviceProfile> findByUserId(Long userId);
 }
--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.PolicyRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
-    Optional<PolicyRule> findByRuleCode(String ruleCode);
     List<PolicyRule> findByActiveTrue();
 }
