ec85e964580f9bb1964c4f2efcf4f6d112f99307
bala-amypo <bala@amypo.in>
2025-12-23 13:46:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ViolationRecordServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.ViolationRecord;
+import com.example.demo.repository.ViolationRecordRepository;
+import com.example.demo.service.ViolationRecordService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ViolationRecordServiceImpl implements ViolationRecordService {
+
+    private final ViolationRecordRepository violationRecordRepository;
+
+    public ViolationRecordServiceImpl(ViolationRecordRepository violationRecordRepository) {
+        this.violationRecordRepository = violationRecordRepository;
+    }
+
+    @Override
+    public ViolationRecord log(ViolationRecord record) {
+        return violationRecordRepository.save(record);
+    }
+}
