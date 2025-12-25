f6d512c69ffd40ce7695eb81d64c69617b498d4e
bala-amypo <bala@amypo.in>
2025-12-25 15:56:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/ViolationRecordController.java
+++ src/main/java/com/example/demo/controller/ViolationRecordController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.ViolationRecord;
 import com.example.demo.service.ViolationRecordService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity; // <-- add this import
+
 
 import java.util.List;
 
