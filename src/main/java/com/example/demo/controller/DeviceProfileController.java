078c273b6fcc7ec5552d27ad854330c89c49c285
bala-amypo <bala@amypo.in>
2025-12-26 03:40:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceProfileController.java
+++ src/main/java/com/example/demo/controller/DeviceProfileController.java
@@ -8,29 +8,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/devices")
-public class DeviceController {
+public class DeviceProfileController {
 
     private final DeviceProfileService deviceService;
 
-    public DeviceController(DeviceProfileService deviceService) {
+    public DeviceProfileController(DeviceProfileService deviceService) {
         this.deviceService = deviceService;
     }
 
-    // POST – add device (multiple times allowed)
     @PostMapping
-    public DeviceProfile create(@RequestBody DeviceProfile device) {
-        return deviceService.create(device);
+    public DeviceProfile add(@RequestBody DeviceProfile device) {
+        return deviceService.addDevice(device);
     }
 
-    // GET by ID – no 404 mapping issue
     @GetMapping("/{id}")
     public DeviceProfile getById(@PathVariable Long id) {
-        return deviceService.getById(id);
+        return deviceService.getDeviceById(id);
     }
 
-    // GET all
     @GetMapping
     public List<DeviceProfile> getAll() {
-        return deviceService.getAll();
+        return deviceService.getAllDevices();
     }
 }
